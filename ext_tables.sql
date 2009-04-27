30ad2cb57831508bb3fc5bbc79d60a90352a31d3
thernst <thernst@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-27 20:13:29 +0000

    Upload version 0.2.6 to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/register4cal/tags/Version_0-2-6@19576 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_tables.sql
@@ -0,0 +1,37 @@
+#
+# Table structure for table 'tx_register4cal_registrations'
+#
+CREATE TABLE tx_register4cal_registrations (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	recordlabel tinytext,
+	cal_event_uid int(11) unsigned DEFAULT '0' NOT NULL,
+	cal_event_getdate int(11) DEFAULT '0' NOT NULL,
+	feuser_uid tinytext,
+	additional_data text,
+	
+	PRIMARY KEY (uid),
+	KEY parent (pid)
+);
+
+
+
+#
+# Table structure for table 'tx_cal_event'
+#
+CREATE TABLE tx_cal_event (
+	tx_register4cal_activate tinyint(3) DEFAULT '0' NOT NULL,
+	tx_register4cal_regstart int(11) DEFAULT '0' NOT NULL,
+	tx_register4cal_regend int(11) DEFAULT '0' NOT NULL,
+);
+
+#
+# Table structure for table 'tx_cal_organizer'
+#
+CREATE TABLE tx_cal_organizer (
+	tx_register4cal_feUserId tinytext
+);
