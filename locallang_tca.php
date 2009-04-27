6687cc2c35e4a2ab2dbc854ea18e6d694ca96c81
thernst <thernst@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-27 20:13:40 +0000

    Upload version 0.2.6 to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/register4cal/tags/Version_0-2-6@19579 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ locallang_tca.php
@@ -0,0 +1,18 @@
+<?php
+	$LOCAL_LANG = Array (
+		'default' => Array (
+			'register4cal.pi1_flexform.label_pages' => 'Storage page for registrations',
+			'register4cal.pi1_flexform.label_recursive' => 'Recursive',
+			'register4cal.pi1_flexform.label_displayMode' => 'What should be displayed',
+			'register4cal.pi1_flexform.label_displayMode.I.2' => 'Registrations of the frontend user',
+			'register4cal.pi1_flexform.label_displayMode.I.3' => 'Registrations for events of the frontend user',
+		),
+		'de' => Array (
+			'register4cal.pi1_flexform.label_pages' => 'Ablageordner für Anmeldungen',
+			'register4cal.pi1_flexform.label_recursive' => 'Rekursiv',
+			'register4cal.pi1_flexform.label_displayMode' => 'Was soll angezeigt werden',
+			'register4cal.pi1_flexform.label_displayMode.I.2' => 'Anmeldungen des Frontend-Benutzers',
+			'register4cal.pi1_flexform.label_displayMode.I.3' => 'Anmeldungen zu Terminen des Frontend-Benutzers',
+		),
+	);
+?>
