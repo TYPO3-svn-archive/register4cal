87f204da51bafb45dbb12a1e4e58236ca3ff3e33
thernst <thernst@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-08 05:18:34 +0000

    [Feature #12860] Page where userdefined fields are stored should be configurable
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/register4cal/trunk@43252 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -1,147 +1,155 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
-	die ('Access denied.');
+
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
 }
 
-$TCA['tx_register4cal_registrations'] = array (
-	'ctrl' => array (
-		'title'     => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations',		
-		'label'     => 'recordlabel',
-		'tstamp'    => 'tstamp',
-		'crdate'    => 'crdate',
+// get extension confArr
+$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['register4cal']);
+// page where records will be stored
+$sPid = '=###CURRENT_PID### ';
+if (isset($confArr['fieldStoragePid'])) {
+	$fieldStoragePid = intval($confArr['fieldStoragePid']);
+	if ($fieldStoragePid != 0) $sPid = ' IN (###CURRENT_PID###,' . $fieldStoragePid . ' )';
+}
+
+$TCA['tx_register4cal_registrations'] = array(
+	'ctrl' => array(
+		'title' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations',
+		'label' => 'recordlabel',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'default_sortby' => 'ORDER BY crdate',	
-		'delete' => 'deleted',	
-		'readOnly' => 0, 
-		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
-		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_register4cal_registrations.gif',
+		'default_sortby' => 'ORDER BY crdate',
+		'delete' => 'deleted',
+		'readOnly' => 0,
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tx_register4cal_registrations.gif',
 	),
 );
 
-$TCA['tx_register4cal_fields'] = array (
-	'ctrl' => array (
-		'title'     => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields',		
-		'label'     => 'caption',	
-		'tstamp'    => 'tstamp',
-		'crdate'    => 'crdate',
+$TCA['tx_register4cal_fields'] = array(
+	'ctrl' => array(
+		'title' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields',
+		'label' => 'caption',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'type' => 'type',	
-		'languageField'            => 'sys_language_uid',	
-		'transOrigPointerField'    => 'l10n_parent',	
-		'transOrigDiffSourceField' => 'l10n_diffsource',	
-		'default_sortby' => 'ORDER BY caption',	
-		'delete' => 'deleted',	
-		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
-		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_register4cal_fields.gif',
+		'type' => 'type',
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'default_sortby' => 'ORDER BY caption',
+		'delete' => 'deleted',
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tx_register4cal_fields.gif',
 	),
 );
 
-$TCA['tx_register4cal_fieldsets'] = array (
-	'ctrl' => array (
-		'title'     => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fieldsets',		
-		'label'     => 'name',	
-		'tstamp'    => 'tstamp',
-		'crdate'    => 'crdate',
+$TCA['tx_register4cal_fieldsets'] = array(
+	'ctrl' => array(
+		'title' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fieldsets',
+		'label' => 'name',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'default_sortby' => 'ORDER BY crdate',	
-		'delete' => 'deleted',	
-		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
-		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_register4cal_fieldsets.gif',
+		'default_sortby' => 'ORDER BY crdate',
+		'delete' => 'deleted',
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tx_register4cal_fieldsets.gif',
 	),
 );
 
-$tempColumns = array (
-	'tx_register4cal_activate' => array (		
-		'exclude' => 0,		
-		'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_activate',		
-		'config' => array (
+$tempColumns = array(
+	'tx_register4cal_activate' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_activate',
+		'config' => array(
 			'type' => 'check',
 		)
 	),
-	'tx_register4cal_regstart' => array (		
-		'exclude' => 0,		
+	'tx_register4cal_regstart' => array(
+		'exclude' => 0,
 		'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_regstart',
 		'displayCond' => 'FIELD:tx_register4cal_activate:REQ:true',
-		'config' => array (
-			'type'     => 'input',
-			'size'     => '8',
-			'max'      => '20',
-			'eval'     => 'date',
+		'config' => array(
+			'type' => 'input',
+			'size' => '8',
+			'max' => '20',
+			'eval' => 'date',
 			'checkbox' => '0',
-			'default'  => '0'
+			'default' => '0'
 		)
 	),
-	'tx_register4cal_regend' => array (		
-		'exclude' => 0,		
+	'tx_register4cal_regend' => array(
+		'exclude' => 0,
 		'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_regend',
 		'displayCond' => 'FIELD:tx_register4cal_activate:REQ:true',
-		'config' => array (
-			'type'     => 'input',
-			'size'     => '8',
-			'max'      => '20',
-			'eval'     => 'date',
+		'config' => array(
+			'type' => 'input',
+			'size' => '8',
+			'max' => '20',
+			'eval' => 'date',
 			'checkbox' => '0',
-			'default'  => '0'
+			'default' => '0'
 		)
 	),
-	'tx_register4cal_maxattendees' => array (		
-		'exclude' => 0,		
+	'tx_register4cal_maxattendees' => array(
+		'exclude' => 0,
 		'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_maxattendees',
 		'displayCond' => 'FIELD:tx_register4cal_activate:REQ:true',
-		'config' => array (
-			'type'     => 'input',
-			'size'     => '8',
-			'max'      => '10',
-			'eval'     => 'num',
+		'config' => array(
+			'type' => 'input',
+			'size' => '8',
+			'max' => '10',
+			'eval' => 'num',
 			'checkbox' => '0',
-			'default'  => '0'
+			'default' => '0'
 		)
 	),
-
-	'tx_register4cal_waitlist' => array (		
-		'exclude' => 0,		
+	'tx_register4cal_waitlist' => array(
+		'exclude' => 0,
 		'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_waitlist',
 		'displayCond' => 'FIELD:tx_register4cal_activate:REQ:true',
-		'config' => array (
+		'config' => array(
 			'type' => 'check',
 		)
 	),
-
-	'tx_register4cal_fieldset' => array (		
-		'exclude' => 0,		
+	'tx_register4cal_fieldset' => array(
+		'exclude' => 0,
 		'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_fieldset',
-		'displayCond' => 'FIELD:tx_register4cal_activate:REQ:true',  
-		'config' => array (
+		'displayCond' => 'FIELD:tx_register4cal_activate:REQ:true',
+		'config' => array(
 			'type' => 'select',
-			'items' => Array (
+			'items' => Array(
 				Array('LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_fieldset.option.2', -2),
 				Array('LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_fieldset.option.1', -1),
-				Array('LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_fieldset.option.0','--div--'),
+				Array('LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_fieldset.option.0', '--div--'),
 			),
-			'foreign_table' => 'tx_register4cal_fieldsets',	
-			'foreign_table_where' => 'AND tx_register4cal_fieldsets.pid=###CURRENT_PID### ORDER BY tx_register4cal_fieldsets.name',	
-			'size' => 1,	
+			'foreign_table' => 'tx_register4cal_fieldsets',
+			'foreign_table_where' => 'AND tx_register4cal_fieldsets.pid' . $sPid . ' ORDER BY tx_register4cal_fieldsets.name',
+			'size' => 1,
 			'minitems' => 1,
 			'maxitems' => 1,
 			'default' => -1,
 		)
-	),	
+	),
 );
 
 t3lib_div::loadTCA('tx_cal_event');
-t3lib_extMgm::addTCAcolumns('tx_cal_event',$tempColumns,1);
-t3lib_extMgm::addToAllTCAtypes('tx_cal_event','--div--;LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_tablabel,tx_register4cal_activate;;;;1-1-1, tx_register4cal_fieldset, tx_register4cal_regstart, tx_register4cal_regend, tx_register4cal_maxattendees, tx_register4cal_waitlist');
+t3lib_extMgm::addTCAcolumns('tx_cal_event', $tempColumns, 1);
+t3lib_extMgm::addToAllTCAtypes('tx_cal_event', '--div--;LLL:EXT:register4cal/locallang_db.xml:tx_cal_event.tx_register4cal_tablabel,tx_register4cal_activate;;;;1-1-1, tx_register4cal_fieldset, tx_register4cal_regstart, tx_register4cal_regend, tx_register4cal_maxattendees, tx_register4cal_waitlist');
 $TCA['tx_cal_event']['ctrl']['requestUpdate'] .= ',tx_register4cal_activate';
 
-$tempColumns = Array (
-	'tx_register4cal_feUserId' => array (		
-		'exclude' => 1,		
-		'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_cal_organizer.tx_register4cal_feUserId',		
-		'config' => Array (
-			'type' => 'group',	
-			'internal_type' => 'db',	
-			'allowed' => 'fe_users, fe_groups',	
-			'size' => 5,	
+$tempColumns = Array(
+	'tx_register4cal_feUserId' => array(
+		'exclude' => 1,
+		'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_cal_organizer.tx_register4cal_feUserId',
+		'config' => Array(
+			'type' => 'group',
+			'internal_type' => 'db',
+			'allowed' => 'fe_users, fe_groups',
+			'size' => 5,
 			'minitems' => 0,
 			'maxitems' => 5,
 		)
@@ -149,16 +157,16 @@ $tempColumns = Array (
 );
 
 t3lib_div::loadTCA('tx_cal_organizer');
-t3lib_extMgm::addTCAcolumns('tx_cal_organizer',$tempColumns,1);
-t3lib_extMgm::addToAllTCAtypes('tx_cal_organizer','tx_register4cal_feUserId;;;;1-1-1');
+t3lib_extMgm::addTCAcolumns('tx_cal_organizer', $tempColumns, 1);
+t3lib_extMgm::addToAllTCAtypes('tx_cal_organizer', 'tx_register4cal_feUserId;;;;1-1-1');
 
 t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages,recursive';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1','FILE:EXT:'.$_EXTKEY.'/pi1/flexform_ds.xml');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages,recursive';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/pi1/flexform_ds.xml');
 
-t3lib_extMgm::addPlugin(array('LLL:EXT:register4cal/locallang_db.xml:tt_content.list_type_pi1',$_EXTKEY.'_pi1'),'list_type');
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/basic/', 'Basic settings');
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/fe-editing/', 'FE-Editing');
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/old_templates/', 'Compatibility to old templates');
+t3lib_extMgm::addPlugin(array('LLL:EXT:register4cal/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY . '_pi1'), 'list_type');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/basic/', 'Basic settings');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/fe-editing/', 'FE-Editing');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/old_templates/', 'Compatibility to old templates');
 ?>
--- tca.php
+++ tca.php
@@ -1,102 +1,113 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-$TCA['tx_register4cal_registrations'] = array (
+if (!defined('TYPO3_MODE')) die('Access denied.');
+
+// get extension confArr
+$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['register4cal']);
+// page where records will be stored
+$sPid = '=###CURRENT_PID### ';
+if (isset($confArr['fieldStoragePid'])) {
+	$fieldStoragePid = intval($confArr['fieldStoragePid']);
+	if ($fieldStoragePid != 0) $sPid = ' IN (###CURRENT_PID###,' . $fieldStoragePid . ' )';
+}
+
+
+$TCA['tx_register4cal_registrations'] = array(
 	'ctrl' => $TCA['tx_register4cal_registrations']['ctrl'],
-	'interface' => array (
+	'interface' => array(
 		'showRecordFieldList' => 'cal_event_uid,feuser_uid,additional_data'
 	),
 	'feInterface' => $TCA['tx_register4cal_registrations']['feInterface'],
-	'columns' => array (
-		'recordlabel' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.recordlabel',		
-			'config' => array (
+	'columns' => array(
+		'recordlabel' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.recordlabel',
+			'config' => array(
 				'type' => 'none',
 			)
 		),
-		'cal_event_uid' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.cal_event_uid',		
-			'config' => array (
+		'cal_event_uid' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.cal_event_uid',
+			'config' => array(
 				'type' => 'group',
 				'internal_type' => 'db',
 				'allowed' => 'tx_cal_event',
 				'size' => 1,
 				'minitems' => 1,
-				'maxitems' =>1,
-				'readOnly' => 1, 
+				'maxitems' => 1,
+				'readOnly' => 1,
 			)
 		),
-		'feuser_uid' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.feuser_uid',		
-			'config' => array (
+		'feuser_uid' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.feuser_uid',
+			'config' => array(
 				'type' => 'group',
 				'internal_type' => 'db',
 				'allowed' => 'fe_users',
 				'size' => 1,
 				'minitems' => 1,
-				'maxitems' =>1,
-				'readOnly' => 1, 
+				'maxitems' => 1,
+				'readOnly' => 1,
 			)
 		),
-		'status' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.status',		
-			'config' => array (
+		'status' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.status',
+			'config' => array(
 				'type' => 'select',
 				'size' => 1,
 				'minitems' => 1,
-				'maxitems' =>1,
-				'readOnly' => 1, 
-				'items' => Array (
+				'maxitems' => 1,
+				'readOnly' => 1,
+				'items' => Array(
 					Array('LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.status.1', 1),
 					Array('LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.status.2', 2),
 					Array('LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.status.3', 3),
 				)
 			)
 		),
-		'additional_data' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.additional_data',		
-			'config' => array (
+		'additional_data' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.additional_data',
+			'config' => array(
 				'type' => 'user',
 				'userFunc' => 'tx_register4cal_backend_hooks->additionalDataForBackend',
 			)
 		),
 		'numattendees' => array(
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.numattendees',		
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_registrations.numattendees',
 			'l10n_mode' => 'exclude',
-			'config' => array (
-				'type' => 'input',	
-				'size' => '5',	
+			'config' => array(
+				'type' => 'input',
+				'size' => '5',
 				'readOnly' => 1,
 			)
 		),
 	),
-	'types' => array (
+	'types' => array(
 		'0' => array('showitem' => 'cal_event_uid;;;;1-1-1, feuser_uid, status, numattendees, additional_data')
 	),
-	'palettes' => array (
+	'palettes' => array(
 		'1' => array('showitem' => '')
 	)
 );
 
-$TCA['tx_register4cal_fields'] = array (
+$TCA['tx_register4cal_fields'] = array(
 	'ctrl' => $TCA['tx_register4cal_fields']['ctrl'],
-	'interface' => array (
+	'interface' => array(
 		'showRecordFieldList' => 'sys_language_uid,l10n_parent,l10n_diffsource,name,caption,type,options,width,height,isnumparticipants'
 	),
 	'feInterface' => $TCA['tx_register4cal_fields']['feInterface'],
-	'columns' => array (
-		'sys_language_uid' => array (		
+	'columns' => array(
+		'sys_language_uid' => array(
 			'exclude' => 1,
-			'label'  => 'LLL:EXT:lang/locallang_general.xml:LGL.language',
-			'config' => array (
-				'type'                => 'select',
-				'foreign_table'       => 'sys_language',
+			'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.language',
+			'config' => array(
+				'type' => 'select',
+				'foreign_table' => 'sys_language',
 				'foreign_table_where' => 'ORDER BY sys_language.title',
 				'items' => array(
 					array('LLL:EXT:lang/locallang_general.xml:LGL.allLanguages', -1),
@@ -104,161 +115,161 @@ $TCA['tx_register4cal_fields'] = array (
 				)
 			)
 		),
-		'l10n_parent' => array (		
+		'l10n_parent' => array(
 			'displayCond' => 'FIELD:sys_language_uid:>:0',
-			'exclude'     => 1,
-			'label'       => 'LLL:EXT:lang/locallang_general.xml:LGL.l18n_parent',
-			'config'      => array (
-				'type'  => 'select',
-				'items' => array (
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.l18n_parent',
+			'config' => array(
+				'type' => 'select',
+				'items' => array(
 					array('', 0),
 				),
-				'foreign_table'       => 'tx_register4cal_fields',
+				'foreign_table' => 'tx_register4cal_fields',
 				'foreign_table_where' => 'AND tx_register4cal_fields.pid=###CURRENT_PID### AND tx_register4cal_fields.sys_language_uid IN (-1,0)',
 			)
 		),
-		'l10n_diffsource' => array (		
-			'config' => array (
+		'l10n_diffsource' => array(
+			'config' => array(
 				'type' => 'passthrough'
 			)
 		),
-		'name' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.name',		
+		'name' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.name',
 			'l10n_mode' => 'exclude',
-			'config' => array (
-				'type' => 'input',	
-				'size' => '30',	
+			'config' => array(
+				'type' => 'input',
+				'size' => '30',
 				'eval' => 'required',
 			)
 		),
-		'caption' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.caption',		
+		'caption' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.caption',
 			'l10n_mode' => 'prefixLangTitle',
-			'config' => array (
-				'type' => 'input',	
-				'size' => '30',	
+			'config' => array(
+				'type' => 'input',
+				'size' => '30',
 				'eval' => 'required',
 			)
 		),
-		'type' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.type',		
+		'type' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.type',
 			'l10n_mode' => 'exclude',
-			'config' => array (
+			'config' => array(
 				'type' => 'select',
-				'items' => array (
-					array('','0'),
+				'items' => array(
+					array('', '0'),
 					array('LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.type.I.1', '1'),
 					array('LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.type.I.2', '2'),
 					array('LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.type.I.3', '3'),
 				),
-				'size' => 1,	
+				'size' => 1,
 				'maxitems' => 1,
 			)
 		),
-		'options' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.options',		
+		'options' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.options',
 			'l10n_mode' => 'prefixLangTitle',
-			'config' => array (
-				'type' => 'input',	
+			'config' => array(
+				'type' => 'input',
 				'size' => '100',
 				'eval' => 'required',
 			)
 		),
-		'defaultvalue' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.defaultvalue',		
+		'defaultvalue' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.defaultvalue',
 			'l10n_mode' => 'prefixLangTitle',
-			'config' => array (
-				'type' => 'input',	
+			'config' => array(
+				'type' => 'input',
 				'size' => '100',
 			)
-		),		
-		'width' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.width',		
+		),
+		'width' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.width',
 			'l10n_mode' => 'exclude',
-			'config' => array (
-				'type' => 'input',	
-				'size' => '3',	
-				'range' => array ('lower'=>1,'upper'=>200),	
+			'config' => array(
+				'type' => 'input',
+				'size' => '3',
+				'range' => array('lower' => 1, 'upper' => 200),
 				'eval' => 'int',
 			)
 		),
-		'height' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.height',		
+		'height' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.height',
 			'l10n_mode' => 'exclude',
-			'config' => array (
-				'type' => 'input',	
-				'size' => '3',	
-				'range' => array ('lower'=>1, 'upper'=>20),	
+			'config' => array(
+				'type' => 'input',
+				'size' => '3',
+				'range' => array('lower' => 1, 'upper' => 20),
 				'eval' => 'int',
 			)
 		),
-		'isnumparticipants' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.isnumparticipants',		
+		'isnumparticipants' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fields.isnumparticipants',
 			'l10n_mode' => 'exclude',
-			'config' => array (
+			'config' => array(
 				'type' => 'check',
 			)
 		),
 	),
-	'types' => array (
+	'types' => array(
 		'0' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, type'),
 		'1' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, name, caption, type, defaultvalue, width, isnumparticipants'),
 		'2' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, name, caption, type, defaultvalue, width, height'),
 		'3' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, name, caption, type, options, defaultvalue, width, height'),
 	),
-	'palettes' => array (
+	'palettes' => array(
 		'1' => array('showitem' => '')
 	)
 );
 
-$TCA['tx_register4cal_fieldsets'] = array (
+$TCA['tx_register4cal_fieldsets'] = array(
 	'ctrl' => $TCA['tx_register4cal_fieldsets']['ctrl'],
-	'interface' => array (
+	'interface' => array(
 		'showRecordFieldList' => 'name,fields,isdefault'
 	),
 	'feInterface' => $TCA['tx_register4cal_fieldsets']['feInterface'],
-	'columns' => array (
-		'name' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fieldsets.name',		
-			'config' => array (
-				'type' => 'input',	
-				'size' => '30',	
+	'columns' => array(
+		'name' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fieldsets.name',
+			'config' => array(
+				'type' => 'input',
+				'size' => '30',
 				'eval' => 'required',
 			)
 		),
-		'fields' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fieldsets.fields',		
-			'config' => array (
-				'type' => 'select',	
-				'foreign_table' => 'tx_register4cal_fields',	
-				'foreign_table_where' => 'AND tx_register4cal_fields.sys_language_uid in (0,-1) AND tx_register4cal_fields.pid=###CURRENT_PID### ORDER BY tx_register4cal_fields.name',	
-				'size' => 10,	
+		'fields' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fieldsets.fields',
+			'config' => array(
+				'type' => 'select',
+				'foreign_table' => 'tx_register4cal_fields',
+				'foreign_table_where' => 'AND tx_register4cal_fields.sys_language_uid in (0,-1) AND tx_register4cal_fields.pid'.$sPid.' ORDER BY tx_register4cal_fields.name',
+				'size' => 10,
 				'minitems' => 1,
 				'maxitems' => 20,
 			)
 		),
-		'isdefault' => array (		
-			'exclude' => 0,		
-			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fieldsets.isdefault',		
-			'config' => array (
+		'isdefault' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:register4cal/locallang_db.xml:tx_register4cal_fieldsets.isdefault',
+			'config' => array(
 				'type' => 'check',
 			)
 		),
 	),
-	'types' => array (
+	'types' => array(
 		'0' => array('showitem' => 'name;;;;1-1-1, fields, isdefault')
 	),
-	'palettes' => array (
+	'palettes' => array(
 		'1' => array('showitem' => '')
 	)
 );
